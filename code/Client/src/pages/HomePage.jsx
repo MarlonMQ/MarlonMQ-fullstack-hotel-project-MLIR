c4408703a00bbe1f64ba996e9b3d70fd83135570
RandySV <randy.sancho@ucr.ac.cr>
2024-04-20 17:38:51 -0600

    estructuraccion de clases titulos, texto, paleta de colores, fonts de tailwind

--- code/Client/src/pages/HomePage.jsx
+++ code/Client/src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import '../index.css'
 
  function HomePage() {
   return (
-    <div className='bg-black min-h-screen items-center  text-center'>
-        <strong className='text-white text-center'>Prueba Pruebita</strong>
+    <div className='bg-violet-100 min-h-screen items-center bg-cover text-center '>
+      <h1 className='primary-title text-white px-12 py-12 bg-primary'>HAZBIN HOTEL</h1>
+      <h2 className='secondary-title text-white py-8 bg-secondary'>Welcome to the Hazbin Hotel</h2>
+      <h3 className='tertiary-title bg-third text-white py-6'>We are excited to have you here</h3>
+      <p className='primary-text bg-fourth text-white py-4'>We hope you enjoy your stay</p>
+      <p className='testimony-text  bg-black text-white'> Andrea</p>
+
     </div>
   )
 }
 
 export default HomePage
+
+
+
--- code/Client/tailwind.config.js
+++ code/Client/tailwind.config.js
@@ -1,9 +1,23 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"],
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {  
+        roboto: ['Roboto', 'sans-serif'],  
+        Jersey: ['Jersey', 'sans-serif'],
+      },
+      colors: {
+        'primary': '#0A1128',
+        'secondary': '#001F54',
+        'third': '#034078',
+        'fourth': '#7dd3fc',
+
+
+      },
+    },
   },
   plugins: [],
 }
