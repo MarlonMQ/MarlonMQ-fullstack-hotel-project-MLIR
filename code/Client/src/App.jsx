1af3f9990f171be34427174d8c4a7682f926cd29
MarlonMQ <105015034+MarlonMQ@users.noreply.github.com>
2024-04-22 12:30:38 -0600

    call to SignUpPage

--- code/Client/src/App.jsx
+++ code/Client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
 import Register from './pages/Register'
+import SignUpPage from './pages/SignUpPage'
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Register />
+      <SignUpPage />
     </>
   )
 
