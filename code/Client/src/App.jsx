c7ce2621a3c6d46890d9bf3f9c8d2ab3868d0340
Ignacio Sanchez <jose.sanchezzeledon@ucr.ac.cr>
2024-04-22 13:27:36 -0600

    Arreglo responsivo

--- code/Client/src/App.jsx
+++ code/Client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
-import Register from './pages/Register'
 import SignUpPage from './pages/SignUpPage'
 
 
--- code/Client/src/pages/SignUpPage.jsx
+++ code/Client/src/pages/SignUpPage.jsx
@@ -4,29 +4,24 @@ import '../index.css'
 
 function SignUpPage() {
     return (
-      <div className="flex flex-col sm:flex-row h-screen min-w-max">
-        <div className="w-full sm:w-1/2 bg-white p-8 flex flex-col justify-center items-center relative">
+      <div className="flex flex-col sm:flex-row h-screen">
+        <div className="w-full mb-20 sm:w-1/2 bg-white p-8 flex flex-col justify-center items-center">
           <div className="text-center">
-            <h1 className='primary-title text-black px-12 py-4'>HAZBIN HOTEL</h1>
-            <h2 className="secondary-title text-black py-8">Sign Up</h2>
+            <h1 className='primary-title text-black px-12 py-12'>HAZBIN HOTEL</h1>
           </div>
-          <div className="bg-gray-300 rounded-lg h-3/4 sm:w-3/5 flex justify-center items-center shadow-2xl p-8">
-            <form className="flex flex-col space-y-4 w-full">
-              <input className="px-4 py-2 rounded bg-white" type="text" placeholder="Nombre" />
-              <input className="px-4 py-2 rounded bg-white" type="text" placeholder="Apellidos" />
-              <input className="px-4 py-2 rounded bg-white" type="email" placeholder="Correo Electrónico" />
-              <input className="px-4 py-2 rounded bg-white" type="date" placeholder="Fecha de Nacimiento" />
-              <input className="px-4 py-2 rounded bg-white" type="password" placeholder="Contraseña" />
-              <input className="px-4 py-2 rounded bg-white" type="text" placeholder="Nacionalidad" />
-              <input className="px-4 py-2 rounded bg-white" type="tel" placeholder="Teléfono" />
-              <input className="px-4 py-2 rounded bg-white" type="text" placeholder="Dirección" />
-              <input className="px-4 py-2 rounded bg-white" type="text" placeholder="Estado" />
-              <input className="px-4 py-2 rounded bg-white" type="text" placeholder="País" />
-              <button type="submit" className="bg-white text-blue-500 px-4 py-2 rounded mt-4">Registrarse</button>
-            </form>
+          <div className="bg-gray-300 rounded-lg h-3/5 w-4/5  flex-col justify-center p-8 shadow-lg shadow-gray-500">
+            <h2 className="secondary-title text-center py-5">Sign Up</h2>
+
+            <div className="items-center text-center ">
+              <p>test</p>
+            </div>
           </div>
         </div>
   
+
+
+
+
         <div className="hidden sm:block sm:w-1/2 justify-center items-center">
           <img
             src={'src/assets/images/fachada.jpg'}
