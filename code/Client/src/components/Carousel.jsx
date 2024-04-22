2e961daef4135c95f209a00137f54bbbc18deade
MLSC123 <marvinsandi3@gmail.com>
2024-04-22 16:59:53 -0600

    responsive design of main caroussel and navbar completed

--- code/Client/src/components/Carousel.jsx
+++ code/Client/src/components/Carousel.jsx
@@ -8,24 +8,47 @@ export const CarouselComponent = () => {
 
         <Carousel autoplay>
               <div>
-                <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image01.jpg" alt="hotel image 01" />
+
+                <div className='hidden block'>
+                  <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image01.jpg" alt="hotel image 01" />
+                </div>
+                <div className="block sm:hidden">
+                  <img className='brightness-50 h-screen' src="../src/assets/hotelPictures/Hotel-image05.jpg" alt="hotel image 01" />
+                </div>
+
               </div>
 
               <div>
-                <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image02.jpg" alt="hotel image 02" />
+
+                <div className='hidden sm:block'>
+                  <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image02.jpg" alt="hotel image 02" />
+                </div>
+
+                <div className="block sm:hidden">
+                  <img className='brightness-50 h-screen' src="../src/assets/hotelPictures/Hotel-image06.jpg" alt="hotel image 01" />
+                </div>
+
               </div>
 
               <div>
-                <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image03.jpg" alt="hotel image 03" />
+                <div className='hidden sm:block'>
+                  <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image03.jpg" alt="hotel image 03" />
+                  
+                </div>
+
+                <div className="block sm:hidden">
+                  <img className='brightness-50 h-screen' src="../src/assets/hotelPictures/Hotel-image07.jpg" alt="hotel image 01" />
+                </div>
+
               </div>
         </Carousel>
 
         <div className='absolute inset-y-1/3 left-20 mt-2'>
-          <h2 className='sm:secondary-title text-white sm:mb-2'>WELCOME TO</h2>
-          <h1 className='sm:primary-title primary-title-mobile text-white sm:mb-2'>HAZBIN</h1>
-          <h2 className='sm:secondary-title text-white sm:mb-2'>HOTEL</h2>
-          <p className=' text-xs sm:primary-text text-white sm:mb-3'>One of the best hotels in this country!</p>
-          <button className='text-xs sm:primary-text text-white bg-third sm:py-2 py-1 sm:px-14 px-5 mt-3 rounded-lg hover:bg-secondary'>RESERVE NOW</button>
+          <h2 className='secondary-title text-white mb-2'>WELCOME TO</h2>
+          <h1 className='primary-title primary-title-mobile text-white mb-2'>HAZBIN</h1>
+          <h2 className='secondary-title text-white mb-2'>HOTEL</h2>
+          <p className=' primary-text text-white mb-3'>One of the best hotels in this country!</p>
+          <button className='primary-text text-white bg-third py-2 px-14 mt-3 rounded-lg hover:bg-secondary'>RESERVE NOW</button>
         </div>
       </div>
     </>
