80023ab0fa4ee64cfdb8a093161a3cc62865a684
RandySV <randy.sancho@ucr.ac.cr>
2024-04-22 14:10:11 -0600

    routeo

--- code/Client/src/App.jsx
+++ code/Client/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
+import Facilities from './pages/Facilities'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 
 
 function App() {
 
-
   return (
-    <HomePage/>
+    <Router>
+      {/* Aquí va la navbar */}
+      <Routes>
+        <Route path="/Home" element={<HomePage/>}/>
+        <Route path="/Facilities" element={<Facilities/>}/>
+
+        {/* ruta register */}
+        {/* ruta login */}
+        {/* ruta las demas */}
+
+      </Routes>
+    </Router>
   )
 
 }
---
+++ code/Client/src/pages/Facilities.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Facilities = () => {
+    return (
+        <div>
+            <h1>Facilities</h1>
+            <p>Welcome to our facilities page!</p>
+            {/* Add your facility components or content here */}
+        </div>
+    );
+};
+
+export default Facilities;
