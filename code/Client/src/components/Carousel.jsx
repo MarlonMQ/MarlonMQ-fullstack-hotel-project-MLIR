4714eee609e93d8d259714003583fc47a64ddff1
MLSC123 <marvinsandi3@gmail.com>
2024-04-22 15:44:53 -0600

    text above carousel added

--- code/Client/src/components/Carousel.jsx
+++ code/Client/src/components/Carousel.jsx
@@ -4,22 +4,30 @@ import { Carousel } from 'antd';
 export const CarouselComponent = () => {
   return (
     <>
+      <div className='relative'>
+
         <Carousel autoplay>
-            <div>
-              <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image01.jpg" alt="hotel image 01" />
-            </div>
+              <div>
+                <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image01.jpg" alt="hotel image 01" />
+              </div>
 
-            <div>
-              <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image02.jpg" alt="hotel image 02" />
+              <div>
+                <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image02.jpg" alt="hotel image 02" />
+              </div>
 
-            </div>
+              <div>
+                <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image03.jpg" alt="hotel image 03" />
+              </div>
+        </Carousel>
 
-            <div>
-              <img className='brightness-50' src="../src/assets/hotelPictures/Hotel-image03.jpg" alt="hotel image 03" />
-            </div>
-
-      </Carousel>
-    
+        <div className='absolute inset-y-1/3 left-20 mt-2'>
+          <h2 className='sm:secondary-title secondary-title-mobile  text-white sm:mb-2'>WELCOME TO</h2>
+          <h1 className='sm:primary-title primary-title-mobile text-white sm:mb-2'>HAZBIN</h1>
+          <h2 className='sm:secondary-title secondary-title-mobile text-white sm:mb-2'>HOTEL</h2>
+          <p className=' text-xs sm:primary-text text-white sm:mb-3'>One of the best hotels in this country!</p>
+          <button className='text-xs sm:primary-text text-white bg-third sm:py-2 py-1 sm:px-14 px-5 mt-3 rounded-lg hover:bg-secondary'>RESERVE NOW</button>
+        </div>
+      </div>
     </>
   )
 }
--- code/Client/src/components/NavBar.jsx
+++ code/Client/src/components/NavBar.jsx
@@ -31,14 +31,14 @@ function NavBar() {
                 {/* Boton para desplegar info del disclosure */}
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
+                    {/* Mobile menu button*/}
+                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    {open ? (
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                    </Disclosure.Button>
 
               </div>
 
@@ -145,7 +145,7 @@ function NavBar() {
 
 
           {/* De smalls screens para arriba estara escondido */}
-          <Disclosure.Panel className="sm:hidden"> 
+          <Disclosure.Panel className="sm:hidden bg-secondary rounded-lg"> 
             <div className="space-y-1 px-2 pb-3 pt-2">
 
               {navigation.map((item) => (
