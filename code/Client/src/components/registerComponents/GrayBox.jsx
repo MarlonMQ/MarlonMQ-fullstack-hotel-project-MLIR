b593cb9d7c0122feeb59a8258ca7742ed47405a5
MarlonMQ <105015034+MarlonMQ@users.noreply.github.com>
2024-04-23 17:15:04 -0600

    Add dependencies and update GrayBox component in Client package

--- code/Client/src/components/registerComponents/GrayBox.jsx
+++ code/Client/src/components/registerComponents/GrayBox.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import FormButton from './FormButton'; // Make sure to import the FormButton component
+import { Link } from 'react-router-dom';
+import FormButton from './FormButton';
 
-const GrayBox = ({ title, paragraph, buttonText, hrefLink, hrefText }) => {
+const GrayBox = ({ title, buttonText, hrefLink, hrefText, onButtonClick, children }) => {
   return (
-    <div className="bg-gray-300 rounded-lg h-4/5 w-4/5 grid grid-rows-3 justify-center p-8 shadow-lg shadow-gray-500">
+    <div className="bg-gray-300 rounded-lg h-4/5 w-4/5 min-w-[300px] min-h-[400px] grid grid-rows-3 justify-center p-8 shadow-lg shadow-gray-500">
       <h2 className="secondary-title text-center py-5">{title}</h2>
 
       <div className="flex flex-col justify-center items-center "> 
         {/* Form */}
-        <p>{paragraph}</p>
+        {children}
       </div>
 
       <div className="flex flex-col items-center justify-center"> 
       
         {/* Botón al final */}
-        <FormButton onClick={() => console.log('Button clicked')} children={buttonText}></FormButton>
+        <FormButton onClick={onButtonClick}>{buttonText}</FormButton>
+        <Link to={hrefLink}>{hrefText}</Link>
 
         {/* Términos y condiciones */}
         <div className="text-center pt-3">
           <p className="mb-2 text-xs text-gray-500">Al continuar, aceptas la Política de privacidad y los Términos de uso</p>
         </div>
 
-
-
         {/* Opción para ir a otra página */}
-        <a href={hrefLink} className="text-sm text-blue-500 hover:underline">{hrefText}</a>
+        <Link to={hrefLink} className="text-sm text-blue-500 hover:underline">{hrefText}</Link>
       </div>
     </div>
   );
--- code/Client/src/pages/SignUpPage.jsx
+++ code/Client/src/pages/SignUpPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
 import GrayBox from '../components/registerComponents/GrayBox';
 
-
-
 function SignUpPage() {
+  const [isFirstDivVisible, setIsFirstDivVisible] = useState(true);
+  const [email, setEmail] = useState("");
+
+  const handleButtonClick = () => {
+      if (email.trim() === "" || !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
+          alert("Por favor, introduce un correo electrónico válido.");
+          return;
+      }
+      setIsFirstDivVisible(false);
+  }
+
+  const handleEmailChange = (e) => {
+      setEmail(e.target.value);
+  }
+
     return (
       <div className="flex flex-col sm:flex-row h-screen">
         <div className="w-full mb-20 sm:w-1/2 bg-white p-8 flex flex-col justify-center items-center">
@@ -13,15 +26,27 @@ function SignUpPage() {
 
           <GrayBox
             title="Sign Up"
-            paragraph="test" 
             buttonText="Continuar"
             hrefLink="/SignIn"
             hrefText="¿Ya tienes una cuenta? Inicia sesión"
-          />
-          
+            onButtonClick={handleButtonClick}
+          >
+            <form className="max-w-md mx-auto">
+              {isFirstDivVisible ? (
+                <div className="relative z-0 w-full mb-5 group">
+                  <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-1 w-60 text-sm text-gray-900  rounded-lg border-3 border-gray-300 appearance-none dark:black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer left" placeholder=" " required />
+                  <label htmlFor="floating_email" className="peer-focus:font-medium absolute tertiary-titl text-sm text-black dark:black duration-300 transform -translate-y-6 scale-75 top-3 z-1 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8 left-1">Email address</label>
+                </div>
+              ) : (
+                <div>
 
+                </div>
+                
+                
+              )}
+            </form>
+          </GrayBox>
         </div>
-  
 
         <div className="hidden sm:block sm:w-1/2 justify-center items-center">
           <img
@@ -32,6 +57,6 @@ function SignUpPage() {
         </div>
       </div>
     );
-  }
-  
-  export default SignUpPage;
+}
+
+export default SignUpPage;
