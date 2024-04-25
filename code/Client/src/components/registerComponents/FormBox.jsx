fa08d1b67a26c87476d415173b1f23b78b6b034e
MarlonMQ <105015034+MarlonMQ@users.noreply.github.com>
2024-04-24 21:50:31 -0600

    Refactor FormBox component in FormBox.jsx

--- code/Client/src/components/registerComponents/FormBox.jsx
+++ code/Client/src/components/registerComponents/FormBox.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 
-<<<<<<< HEAD
 const FormBox = ({ title, name, type, error, value, change, blur }) => {
     return (
         <div className=" relative w-full mb-6 group">
-=======
-const FormBox = ({ title, type, error, value, change, blur }) => {
-    return (
-        <div className="relative z-0 w-full mb-5 group">
->>>>>>> 45d52cf0775b8a3522cfbc415580b805784006fe
             <input 
                 value={value} 
                 onChange={change} 
                 id={title}
-<<<<<<< HEAD
                 name={name} 
                 type={type} 
                 onBlur={blur} 
@@ -21,14 +14,6 @@ const FormBox = ({ title, type, error, value, change, blur }) => {
                 aria-labelledby={title} 
                 className={`block py-2.5 px-1 w-full text-sm text-gray-900 rounded-lg border-3 ${error ? 'border-red-500' : 'border-gray-300'} appearance-none dark:black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer left`}  />
             <label htmlFor={title} id={title} className="cursor-text peer-focus:font-medium absolute tertiary-titl text-sm text-gray-500 dark:black duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8 left-1">{title}</label>
-=======
-                name={type} 
-                type={type} 
-                onBlur={blur} 
-                placeholder=" "
-                className={`block py-2.5 px-1 w-full text-sm text-gray-900 rounded-lg border-3 ${error ? 'border-red-500' : 'border-gray-300'} appearance-none dark:black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer left`}  />
-            <label htmlFor="floating_email" className="peer-focus:font-medium absolute tertiary-titl text-sm text-gray-500 dark:black duration-300 transform -translate-y-6 scale-75 top-3 z-1 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8 left-1">{title}</label>
->>>>>>> 45d52cf0775b8a3522cfbc415580b805784006fe
             {error && <p className="text-red-500 text-xs pt-2">{error}</p>}
         </div>
   );
