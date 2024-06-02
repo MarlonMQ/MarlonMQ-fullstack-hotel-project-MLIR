ab08b94e2a8e7ef5ae5927580bbfadc62c3df5f0
MLSC123 <marvinsandi3@gmail.com>
2024-06-02 16:54:38 -0600

    funcionalidad actualizar room implementada

--- code/Client/src/components/Rooms/FormRooms.jsx
+++ code/Client/src/components/Rooms/FormRooms.jsx
@@ -2,25 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
-import { toast } from 'react-toastify';
 
 
-const FormRooms = () => {
-
+const FormRooms = ({valuesForm, setValuesToRoomForm}) => {
 
   /* state and initial values */
   const [imagePreview, setImagePreview] = useState(null);
-
-
+  
   const formik = useFormik({
-  initialValues: {
-    type: '',
-    price: 1,
-    availables: 1,
-    capacity: 1,
-    description: '',
-    image: null,
-  },
+  initialValues: valuesForm,
   
     /* Validation part */
   validationSchema: Yup.object({
@@ -44,11 +34,12 @@ const FormRooms = () => {
       .test("fileFormat", "Unsupported format", value => !value || (value && ["image/jpg", "image/jpeg", "image/gif", "image/png", "image/avif"].includes(value.type)))
   }),
 
-  // send to backend //
+  // send to backend 
   onSubmit: async (values) => {
-    console.log("Received info: ", values);
+    console.log("Onsubmit llamado: ", values);
 
     const formData = new FormData();
+    formData.append('id', values.id);
     formData.append('type', values.type);
     formData.append('price', values.price);
     formData.append('availables', values.availables);
@@ -58,13 +49,34 @@ const FormRooms = () => {
 
 
     try {
-      const response = await axios.post('http://localhost:4000/rooms/', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-      console.log('Room uploaded successfully:', response.data);
-      toast.success('Room uploaded successfully');
+      if (valuesForm.updateMode == 0) {
+
+        console.log("uploading");
+        const response = await axios.post('http://localhost:4000/rooms/', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        console.log('Room uploaded successfully:', response.data);
+        alert('Room uploaded successfully');
+
+      } else {
+        console.log("updating");
+        const response = await axios.patch(`http://localhost:4000/rooms/?url=${valuesForm.image}`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        console.log('Room updated successfully:', response.data);
+        // Vuelve a estar en modo de upload
+        setValuesToRoomForm(prevState => ({
+          ...prevState,
+          updateMode: 0
+        }));
+ 
+        alert('Room updated successfully'); 
+      }
+
 
       
     } catch (error) {
@@ -74,11 +86,11 @@ const FormRooms = () => {
         console.log('Error data:', error.response.data);
         console.log('Error status:', error.response.status);
         console.log('Error headers:', error.response.headers);
-        toast.error(`Error uploading room: ${error.response.data.message}`);
+        alert(`Error uploading room: ${error.response.data.message || 'Unspecified error'}`);
       } else if (error.request) {
         // The request was made but no response was received
         console.log('Request error:', error.request);
-        toast.error('Error uploading room: No response received from the server');
+        alert('Error uploading room: No response received from the server');
       } else {
         // Something happened in setting up the request that triggered an Error
         console.log('Error message:', error.message);
@@ -88,18 +100,38 @@ const FormRooms = () => {
     },
   });
 
-// receive image input //
+// receive image input
   const handleImageChange = (event) => {
-  const file = event.currentTarget.files[0];
-  formik.setFieldValue("image", file);
-  if (file) {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-    setImagePreview(reader.result);
-    };
-    reader.readAsDataURL(file);
-  }
+    const file = event.currentTarget.files[0];
+    console.log("file: ", file);
+    formik.setFieldValue("image", file);
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+    }
   };
+
+  useEffect(() => {
+    console.log("Nuevos valores: ", valuesForm);
+    formik.setValues(valuesForm);
+
+    if (typeof valuesForm.image === 'string') {
+      // Fetch the image from the URL and convert it to a File object
+      fetch(valuesForm.image)
+        .then(res => res.blob())
+        .then(blob => {
+          const file = new File([blob], 'image.jpg', { type: blob.type });
+          formik.setFieldValue('image', file);
+          setImagePreview(URL.createObjectURL(file));
+        })
+        .catch(error => console.error('Error fetching the image:', error));
+    }
+  }, [valuesForm]);
+
 
   return (
   <div className=" mx-auto bg-white p-8 border border-gray-300 mt-12 mb-12">
@@ -190,13 +222,31 @@ const FormRooms = () => {
       onChange={handleImageChange}
       className="mt-1 block w-full px-3 py-2 border border-gray-300 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-fourth hover:file:bg-violet-100"
       />
-      {formik.errors.image && <div className="text-red-500 text-xs italic">{formik.errors.image}</div>}
+      {
+        formik.errors.image && 
+        <div className="text-red-500 text-xs italic">{formik.errors.image} </div>
+      }
       {imagePreview && <img src={imagePreview} alt="Preview" className="mt-4 w-full h-auto rounded-md"/>}
     </div>
+    {
+      (valuesForm.updateMode == 0)
+      ? 
+      <button 
+        type="submit" 
+        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-fourth hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Upload Room
+      </button>
+      : 
+      <button 
+        type="submit"
+        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-400 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        
+      >
+        Update room
+      </button>
+    }
 
-    <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-fourth hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      Upload Room
-    </button>
     </form>
   </div>
   );
