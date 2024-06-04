b5b4f8a744f8bbaed92f8dbc330acaaaeaad0295
MarlonMQ <105015034+MarlonMQ@users.noreply.github.com>
2024-06-03 21:47:53 -0600

    chore: Add nodemailer dependency for email functionality

---
+++ code/Client/src/components/accounts/AccountFormBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AccountFormBox = ({ title, name, type, error, value, change, blur, placeholder }) => {
+    return (
+      <div>
+        <label className="block mb-2 text-sm font-medium text-gray-900">{title}</label>
+          <input
+              value={value}
+              onChange={change}
+              id={title}
+              name={name}
+              type={type}
+              onBlur={blur}
+              placeholder={placeholder}
+              className={`block w-full p-3 border ${error ? 'border-red-500' : 'border-gray-300'} rounded`}
+          />
+          {error && <p className="text-red-500 text-xs italic">{error}</p>}
+      </div>
+  );
+};
+
+export default AccountFormBox;
---
+++ code/Client/src/components/accounts/AccountRolSelector.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const RoleSelector = ({ value, handleChange, error }) => {
+  return (
+    <div>
+      <label className="block mb-2 text-sm font-medium text-gray-900">Role</label>
+      <select
+        name="rol"
+        value={value}
+        onChange={handleChange}
+        className={`block w-full p-3 border ${error ? 'border-red-500' : 'border-gray-300'} rounded`}
+      >
+        <option value="user">Client</option>
+        <option value="employee">Employee</option>
+      </select>
+      {error && <p className="text-red-500 text-xs italic">{error}</p>}
+    </div>
+  );
+};
+
+export default RoleSelector;
