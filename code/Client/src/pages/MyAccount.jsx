1b578903d54732ca118f455210d9b71b4c175cec
MarlonMQ <105015034+MarlonMQ@users.noreply.github.com>
2024-07-03 09:19:04 -0600

    fix: Update response status check in MyAccount page

--- code/Client/src/pages/MyAccount.jsx
+++ code/Client/src/pages/MyAccount.jsx
@@ -143,7 +143,7 @@ function MyAccount() {
       const response = await Axios.post('/myAccount', values);
       setEditMode(false);
       fetchAccount();
-      if (response.status === 201) {
+      if (response.status === 200) {
         updateProfileImage(formik.values.profile_image);
         toast.success('Account updated successfully');
       }
