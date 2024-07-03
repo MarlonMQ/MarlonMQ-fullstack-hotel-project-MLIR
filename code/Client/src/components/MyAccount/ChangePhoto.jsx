c452a1702ab32bb78161fb107033805752218e08
MarlonMQ <105015034+MarlonMQ@users.noreply.github.com>
2024-07-02 23:58:37 -0600

    chore: Refactor ChangePhoto component styles for better readability

--- code/Client/src/components/MyAccount/ChangePhoto.jsx
+++ code/Client/src/components/MyAccount/ChangePhoto.jsx
@@ -4,23 +4,23 @@ function ChangePhoto({onUploadPhoto, onDeletePhoto, onClose }) {
   
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-    <div className="bg-white w-80 pt-5 rounded-md text-center divide-y ">
+    <div className="bg-white w-80 pt-5 rounded-md text-center  ">
       <p className="mb-4 text-xl">Change profile image</p>
-      <div className="w-full flex flex-col justify-end divide-y">
+      <div className="w-full flex flex-col justify-end ">
       <button
-        className="w-full text-fourth  py-2 px-4 rounded-md"
+        className="w-full hover:bg-gray-300 text-fourth  py-2 px-4 "
         onClick={onUploadPhoto}
       >
         Upload image
       </button>  
       <button
-        className="w-full  text-red-600  py-2 px-4 rounded-md"
+        className="w-full hover:bg-gray-300 text-red-600  py-2 px-4 "
         onClick={onDeletePhoto}
       >
         Delete current image
       </button>
       <button
-        className="w-full text-gray-800  py-2 px-4 rounded-md"
+        className="w-full hover:bg-gray-300 text-gray-800  py-2 px-4 "
         onClick={onClose}
       >
         Cancel
